0ca2ece6bd2af67d037a73a624ddb5d6584aecb2
Siivi <46359684+Siivi@users.noreply.github.com>
2019-01-14 22:25:19 +0200

    Delete yl3.py

--- yl3.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Sisesta esimene number: "))
-num2 = int(input("Sisesta teine number: "))
-
-if num1 <num2:
-    print(str(num1) + " on väiksem!")
-else:
-    print(str(num2) + " on väiksem!")
